4fe0e5a5c0c8b46ff5236a263ffc22a7f8f19bca
Radoslaw Lyson <radoslaw.lyson@codetain.com>
2024-09-28 21:05:16 +0200

    feat: base project configuration

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import Fastify from 'fastify';
+import { appConfig } from '../config/app.config';
+
+const server = Fastify({
+  logger: true,
+});
+
+async function main() {
+  try {
+    const config = appConfig();
+
+    await server.listen({
+      port: +config.port,
+      host: config.host,
+    });
+
+    console.log(`Server listening at http://${config.host}:${config.port}`);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+main();
